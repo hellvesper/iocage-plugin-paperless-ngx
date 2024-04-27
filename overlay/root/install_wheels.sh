7a3e17a48ddcca3f021fa9f1bfa878045c5793b2
Vesper <me@vesp.dev>
2024-04-27 15:48:05 +0300

    path fix

--- overlay/root/install_wheels.sh
+++ overlay/root/install_wheels.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-cd wheels
-
 while read requirement; do
     if ! pip3.11 install --no-index --find-links ./wheels/ "$requirement"; then
     exit 1
     fi
 done < ../requirements.txt
-
-cd ..
