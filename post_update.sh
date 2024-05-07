d3c736453aec972dcc2ff596dc4549b0811c5003
Vesper <me@vesp.dev>
2024-05-07 17:40:18 +0300

    update test

--- post_update.sh
+++ post_update.sh
@@ -1,1 +1,37 @@
 #!/bin/sh
+
+
+set username="paperless"
+set fullname="paperless-ngx"
+set appname="paperless-ngx"
+set uid=1000
+set gid=1000
+set home="/home/paperless"
+
+### fetch paperless-ngx
+cd ${home}
+
+sudo -Hu ${username} fetch https://github.com/paperless-ngx/paperless-ngx/releases/download/v2.8.1/paperless-ngx-v2.8.1.tar.xz && \
+sudo -Hu ${username} tar -xf paperless-ngx-v2.8.1.tar.xz
+
+
+### fetch prebuild wheels
+# sudo -Hu ${username} fetch https://github.com/hellvesper/iocage-plugin-paperless-ngx/releases/download/v2.7.2%4013.2-RELEASE/wheels.tar.xz
+sudo -Hu ${username} tar -xf wheels.tar.xz
+
+### rename wheels, because it python store os version from 'uname -r' which is TrueNAS base os version
+cp /root/rename.sh ./
+chown ${username}:${username} rename.sh
+sudo -Hu ${username} /bin/sh rename.sh
+
+### install wheels
+setenv PATH ${PATH}:/home/${username}/.local/bin
+sudo -Hu ${username} python3.11 -m ensurepip --upgrade
+sudo -Hu ${username} cp ${appname}/requirements.txt ./
+sudo -Hu ${username} sed -i '' 1d requirements.txt
+
+cp /root/install_wheels.sh ./
+setenv PATH ${PATH}:/home/${username}/.local/bin
+chown ${username}:${username} install_wheels.sh
+sudo -Hu ${username} /bin/sh install_wheels.sh
+# pip3.11 install --no-build-isolation pyyaml==6.0.1
--- pre_update.sh
+++ pre_update.sh
@@ -1,1 +1,37 @@
 #!/bin/sh
+
+service paperless stop
+# Check if the paperless service has stopped and wait until it has stopped
+while service paperless status | grep -q 'is running'; do
+    echo "Waiting for paperless service to stop..."
+    sleep 1
+done
+echo "Paperless service stopped."
+
+service redis stop
+
+# Check if the redis service has stopped and wait until it has stopped
+while service redis status | grep -q 'is running'; do
+    echo "Waiting for redis service to stop..."
+    sleep 1
+done
+echo "Redis service stopped."
+
+# Check if any Python application is running and kill all instances
+echo "Checking for running Python applications..."
+pgrep -f python | xargs -r kill -9
+echo "All running Python applications have been terminated."
+
+# Remove paperless-ngx tarball with any version number if it exists
+echo "Checking for existing paperless-ngx tarballs to remove..."
+find /home/paperless -type f -name 'paperless-ngx-*.tar.xz' -exec echo "Removing {}" \; -exec rm {} \;
+echo "Removal of paperless-ngx tarballs complete."
+
+echo "Creating backup of the paperless-ngx folder..."
+tar -czf /home/paperless/paperless-ngx.tar.gz /home/paperless/paperless-ngx
+mv /home/paperless/paperless-ngx.tar.gz /home/paperless/paperless-ngx.tar.gz.bak
+echo "Backup created and renamed to paperless-ngx.tar.gz.bak."
+
+echo "Removing the original paperless-ngx folder..."
+rm -rf /home/paperless/paperless-ngx
+echo "Original paperless-ngx folder removed."
