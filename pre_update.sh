be02846736ab2c1b869c8515c3a0a66383ad6178
Vesper <me@vesp.dev>
2024-05-07 21:45:28 +0300

    tcsh

--- pre_update.sh
+++ pre_update.sh
@@ -2,10 +2,10 @@
 
 service paperless stop
 # Check if the paperless service has stopped and wait until it has stopped
-while service paperless status | grep -q 'is running'; do
+while (`service paperless status | grep -q 'is running'`)
     echo "Waiting for paperless service to stop..."
     sleep 1
-done
+end
 echo "Paperless service stopped."
 
 sysrc -f /etc/rc.conf paperless_enable=NO
@@ -13,20 +13,25 @@ sysrc -f /etc/rc.conf paperless_enable=NO
 service redis stop
 
 # Check if the redis service has stopped and wait until it has stopped
-while service redis status | grep -q 'is running'; do
+while (`service redis status | grep -q 'is running'`)
     echo "Waiting for redis service to stop..."
     sleep 1
-done
+end
 echo "Redis service stopped."
 
 # Check if any Python application is running and kill all instances
 echo "Checking for running Python applications..."
-pgrep -f python | xargs -r kill -9
+foreach pid (`pgrep -f python`)
+    kill -9 $pid
+end
 echo "All running Python applications have been terminated."
 
 # Remove paperless-ngx tarball with any version number if it exists
 echo "Checking for existing paperless-ngx tarballs to remove..."
-find /home/paperless -type f -name 'paperless-ngx-*.tar.xz' -exec echo "Removing {}" \; -exec rm {} \;
+foreach file (`find /home/paperless -type f -name 'paperless-ngx-*.tar.xz'`)
+    echo "Removing $file"
+    rm $file
+end
 echo "Removal of paperless-ngx tarballs complete."
 
 echo "Creating backup of the paperless-ngx folder..."
@@ -37,7 +42,6 @@ echo "Backup created and renamed to paperless-ngx.tar.gz.bak."
 echo "Backing up paperless.conf to user home directory..."
 cp /home/paperless/paperless-ngx/paperless.conf /home/paperless/paperless.conf
 echo "Backup of paperless.conf completed."
-
 
 echo "Removing the original paperless-ngx folder..."
 rm -rf /home/paperless/paperless-ngx
